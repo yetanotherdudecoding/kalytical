8c13e00808f12f93c674894ef22bed4671dba755
Bradley Savoy <zerodown524@instance-2.us-east1-b.c.sandbox-224519.internal>
2018-12-17 16:25:14 +0000

    first attempt at spark clustering

--- spark/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker pull gettyimages/spark:latest
-sudo docker tag gettyimages/spark:latest instance-2:8080/spark:201812130
---
+++ spark/worker/startWorker.sh
@@ -0,0 +1,1 @@
+bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_URL
