5dbf9243ef89aebd50bfdfee077aa35038ea16e4
Bradley Savoy <zerodown524@instance-1.us-east1-b.c.sandbox-224519.internal>
2018-12-17 21:44:18 +0000

    expose port for Hdfs outside cluster

--- examples/shakespere_count.py
+++ examples/shakespere_count.py
@@ -1,12 +1,9 @@
+from pyspark.sql import SparkSession
+
 if __name__ == "__main__":
 	spark = SparkSession.builder.appName("Shakespere").getOrCreate()
-
-	df = spark.read.text("hdfs://10.244.0.13:9000/testdata/*")
-
+	df = spark.read.text("hdfs://10.110.34.48:9000/testdata/*")
 	df.count()
-
 	wordCounts = df.select(explose(split(df.value,"\s+")).alias("word")).groupBy("word").count()
-
 	print("Word count statistics: " + wordCounts.collect())
-	
 	spark.stop()
