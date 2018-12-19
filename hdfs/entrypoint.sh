f8fa364843e726479fc829cbbb43e84988f4b044
Bradley Savoy <zerodown524@instance-1.us-east1-b.c.sandbox-224519.internal>
2018-12-19 03:47:30 +0000

    update jupyter to spark-all image. Fixing webui for hdfs

--- hdfs/entrypoint.sh
+++ hdfs/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 /opt/hadoop-2.7.7/bin/hdfs namenode -D &
 /opt/hadoop-2.7.7/bin/hdfs datanode -D &
-/opt/hadoop-2.7.7/bin/hdfs dfs -mkdir /data/
-/opt/hadoop-2.7.7/bin/hdfs dfs -chmod 777 /data
+/opt/hadoop-2.7.7/bin/hdfs dfs -mkdir hdfs://localhost:9000/data/
+/opt/hadoop-2.7.7/bin/hdfs dfs -chmod 777 hdfs://localhost:9000/data/
 while true; do
   sleep 10
 done
