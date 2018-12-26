18e83a7645ff7fe68827e756837c6272a9813485
Bradley Savoy <zerodown524@instance-1.us-east1-b.c.sandbox-224519.internal>
2018-12-26 20:56:33 +0000

    move spark job submission to k8s job api object type

--- spark/buildImages.sh
+++ spark/buildImages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl -L https://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-without-hadoop.tgz -o spark_2.4.0.tar.gz && \
-tar -xzf spark_2.4.0.tar.gz && \
-rm spark_2.4.0.tar.gz && \
-cd spark-2.4.0-bin-without-hadoop && \ 
-docker build . -t instance-1:8080/spark:2.4.0 -f kubernetes/dockerfiles/spark/Dockerfile && \
-docker push instance-1:8080/spark:2.4.0
-cd .. && rm -rf spark-2.4.0-bin-without-hadoop
+curl -L https://archive.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz -o spark_2.3.1.tar.gz && \
+tar -xzf spark_2.3.1.tar.gz && \
+rm spark_2.3.1.tar.gz && \
+cd spark-2.3.1-bin-hadoop2.7 && \ 
+docker build . -t instance-1:8080/spark:2.3.1 -f kubernetes/dockerfiles/spark/Dockerfile && \
+docker push instance-1:8080/spark:2.3.1
+cd .. && rm -rf spark-2.3.1-bin-without-hadoop
---
+++ spark/submitPod/entrypoint.sh
@@ -0,0 +1,2 @@
+echo $SPARK_SUBMIT_ARGS
+/root/spark-2.3.1-bin-hadoop2.7/bin/spark-submit $SPARK_SUBMIT_ARGS
