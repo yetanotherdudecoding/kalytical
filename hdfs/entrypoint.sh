5de0e9623ade211f3b88741a0ca79237215d1aaf
Bradley Savoy <zerodown524@instance-1.us-east1-b.c.sandbox-224519.internal>
2018-12-27 15:57:38 +0000

    fix hdfs default data directories on container creation. Still no clustering support

--- hdfs/entrypoint.sh
+++ hdfs/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 /opt/hadoop-2.7.7/bin/hdfs namenode -D &
 /opt/hadoop-2.7.7/bin/hdfs datanode -D &
-/opt/hadoop-2.7.7/bin/hdfs dfs -mkdir hdfs://localhost:9000/data/
-/opt/hadoop-2.7.7/bin/hdfs dfs -chmod 777 hdfs://localhost:9000/data/
+/opt/hadoop-2.7.7/bin/hdfs dfsadmin -report
+status=$?
+while [ "$status" != "0" ]; do
+       sleep 5
+       /opt/hadoop-2.7.7/bin/hdfs dfsadmin -report
+      status=$?
+      echo "status is:"$status
+done
+#Now we can create our default data directories
+/opt/hadoop-2.7.7/bin/hdfs dfs -mkdir hdfs://localhost:9000/data
+/opt/hadoop-2.7.7/bin/hdfs dfs -chmod 777 hdfs://localhost:9000/data
+/opt/hadoop-2.7.7/bin/hdfs dfs -mkdir hdfs://localhost:9000/data/{raw,prepared,published}
+/opt/hadoop-2.7.7/bin/hdfs dfs -chmod 777 hdfs://localhost:9000/data/{raw,prepared,published}
 while true; do
-  sleep 10
+       sleep 10
 done
