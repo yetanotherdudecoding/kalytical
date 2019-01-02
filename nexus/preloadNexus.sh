79adc143dbcde14b8b61b519a0d0675aed85f7d0
Bradley Savoy <brad.savoy@gmail.com>
2019-01-02 21:55:03 +0000

    can provision all resources. Only dependency on my personal infrastructure is streamsets-all tarball (4GB). Need to test bootstrapping standalone.

--- nexus/preloadNexus.sh
+++ nexus/preloadNexus.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 DOCKER_REGISTRY=$1
 NEXUS_RAW_URL=$2
-#sudo docker pull alpine:3.6
-#sudo docker tag alpine:3.6 $DOCKER_REGISTRY/alpine:3.6 
-#sudo docker push $DOCKER_REGISTRY/alpine:3.6
-#
-#sudo docker pull sonatype/nexus3
-#sudo docker tag sonatype/nexus3 $DOCKER_REGISTRY/sonatype/nexus3
-#sudo docker push $DOCKER_REGISTRY/sonatype/nexus3
-#
-#sudo docker pull openjdk:8-jdk
-#sudo docker tag openjdk:8-jdk $DOCKER_REGISTRY/openjdk:8-jdk
-#sudo docker push $DOCKER_REGISTRY/openjdk:8-jdk
 
-
-IMAGES=('alpine:3.6' 'sonatype/nexus3' 'openjdk:8-jdk' 'centos:7')
+IMAGES=('alpine:3.6' 'sonatype/nexus3' 'openjdk:8-jdk' 'centos:7' 'jupyter/all-spark-notebook')
 
 for i in ${IMAGES[@]}; do
 	sudo docker pull $i
@@ -23,7 +11,7 @@ for i in ${IMAGES[@]}; do
 done
 
 
-ARTIFACT_LIST=('http://ftp.naz.com/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz' 'http://mirrors.ocf.berkeley.edu/apache/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz')
+ARTIFACT_LIST=('http://ftp.naz.com/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz' 'http://mirrors.ocf.berkeley.edu/apache/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz' 'https://archives.streamsets.com/datacollector/3.6.1/tarball/streamsets-datacollector-all-3.6.1.tgz')
 
 for i in ${ARTIFACT_LIST[@]}; do
         ITEM_NAME=$(echo $i | awk 'BEGIN {FS="/"}; {print $NF}')
@@ -34,5 +22,7 @@ for i in ${ARTIFACT_LIST[@]}; do
 	curl -v -u admin:admin123 --upload-file $ITEM_NAME $NEXUS_RAW_URL/raw-arifacts/oss/$ITEM_NAME
 done
 
+#For sample purposes
 tar -xzvf spark-2.3.1-bin-hadoop2.7.tgz spark-2.3.1-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.3.1.jar
 curl -v -u admin:admin123 --upload-file spark-2.3.1-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.3.1.jar $NEXUS_RAW_URL/dataproducts/examples/spark-examples_2.11-2.3.1.jar
+rm -rf spark-2.3.1-bin-hadoop2.7
--- streamsets/README.md
+++ streamsets/README.md
@@ -1,1 +1,3 @@
 Fork of official streamsets docker image build found here https://github.com/streamsets/datacollector-docker/
+Official complete tarball of stremasets - try to precache this if you can because its' rather large 
+https://archives.streamsets.com/datacollector/3.6.1/tarball/streamsets-datacollector-all-3.6.1.tgz
