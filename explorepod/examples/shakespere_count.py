248f3cd2ab0fba172a157e0e6b6a94c90b03a1ad
Bradley Savoy <brad.savoy@gmail.com>
2019-01-03 18:20:31 +0000

    Update shakespeare hadoop word count to work from explorepod

--- explorepod/examples/shakespere_count.py
+++ explorepod/examples/shakespere_count.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 if __name__ == "__main__":
-	spark = SparkSession.builder.appName("Shakespere").getOrCreate()
-	df = spark.read.text("hdfs://10.110.34.48:9000/testdata/*")
-	df.count()
-	wordCounts = df.select(explose(split(df.value,"\s+")).alias("word")).groupBy("word").count()
-	print("Word count statistics: " + wordCounts.collect())
-	spark.stop()
+        spark = SparkSession.builder.appName("Shakespere").getOrCreate()
+        df = spark.read.text("hdfs://hdfs-service:9000/data/raw/ds=shakespeare/*/*")
+        df.count()
+        wordCounts = df.select(explode(split(df.value,"\s+")).alias("word")).groupBy("word").count()
+        print(wordCounts.filter("`count` >= 10").sort(col("count").desc()).show())
+        spark.stop()
